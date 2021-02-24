d020d127e5ca8f5f885714851e11bc54ec46ecd4
RocqJones <jonesmbindyo@gmail.com>
2021-02-24 15:09:18 +0300

    extract ID from a YouTube URL

--- SlicingString/src/Slicing.kt
+++ SlicingString/src/Slicing.kt
@@ -1,2 +1,12 @@
-class Slicing {
+package my.program
+
+fun main(args: Array<String>) {
+    val string = "hello@world.com"
+    val url = "https://youtu.be/snqMchRhQnk"
+
+    val domain: String? = string.substringAfterLast("@")
+    val extractId: String? = url.substringAfterLast("youtu.be/")
+
+    println(domain)
+    println(extractId)
 }
